ba13ddf5f62bfd5dc29319aa8cb1e49d5e8bbc88
Dmitrii Nikolaev <dmitrynikolaev08@gmail.com>
2024-01-21 19:18:24 +0100

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,24 +10,20 @@ type UploadedImage = {
 
 export default function Home() {
     const [uploadedImages, setUploadedImages] = useState<UploadedImage[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
     const fileInputRef = useRef<HTMLInputElement>(null);
 
     const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
         const { files } = e.target;
-        console.log('files', files);
         if (files && files.length > 0) {
             const formData = new FormData();
             const imageFiles = Array.from(files);
-            console.log('imageFiles', imageFiles);
             imageFiles.forEach((image) => {
                 formData.append('image', image);
             });
 
-            formData.forEach((value, key) => {
-                console.log('key', key);
-                console.log('value', value);
-            });
+            setLoading(true);
 
             const res = await fetch('/api/crop-images', {
                 method: 'POST',
@@ -45,8 +41,10 @@ export default function Home() {
                     ),
                     name: image.name,
                 }));
+                setLoading(false);
                 setUploadedImages(croppedImages);
             } else {
+                setLoading(false);
                 setError('Error cropping images');
             }
         }
@@ -73,52 +71,68 @@ export default function Home() {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+        setUploadedImages([]);
     };
 
     const triggerFileInput = () => {
         fileInputRef.current?.click();
     };
 
-    const renderImages = () => {
-        return uploadedImages.map((image, index) => (
-            <div key={index} className="m-4">
-                <img
-                    src={image.src}
-                    alt={image.name}
-                    className="h-auto w-auto"
-                />
-            </div>
-        ));
-    };
+    // const renderImages = () => {
+    //     return uploadedImages.map((image, index) => (
+    //         <div key={index} className="m-4">
+    //             <img
+    //                 src={image.src}
+    //                 alt={image.name}
+    //                 className="h-auto w-auto"
+    //             />
+    //         </div>
+    //     ));
+    // };
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-100">
-            <h1 className="mb-6 text-4xl font-bold">Image Cropper</h1>
-            <button
-                type="button"
-                onClick={triggerFileInput}
-                className="mb-4 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300"
-            >
-                Upload Images
-            </button>
-            <input
-                type="file"
-                ref={fileInputRef}
-                onChange={handleFileChange}
-                multiple
-                className="hidden"
-            />
+            <h1 className="mb-6 text-4xl font-bold">Image Handler v1.0</h1>
+            {(!uploadedImages || uploadedImages?.length === 0 || error) && (
+                <>
+                    <button
+                        type="button"
+                        onClick={triggerFileInput}
+                        className="mb-4 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300 disabled:opacity-50"
+                        disabled={loading}
+                    >
+                        Upload Images
+                    </button>
+                    <input
+                        type="file"
+                        ref={fileInputRef}
+                        onChange={handleFileChange}
+                        multiple
+                        className="hidden"
+                    />
+                </>
+            )}
+
             {error && <p className="text-red-500">{error}</p>}
-            <div className="flex flex-wrap justify-center">
-                {renderImages()}
-            </div>
-            <button
-                type="button"
-                onClick={handleDownloadAll}
-                className="mb-4 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300"
-            >
-                Download All Images
-            </button>
+            {/* <div className="flex flex-wrap justify-center"> */}
+            {/*     {renderImages()} */}
+            {/* </div> */}
+            {loading && <p className="text-blue-500">Loading...</p>}
+            {uploadedImages?.length > 0 && (
+                <>
+                    <span className="font-semibold text-lg mb-3 mt-6">
+                        {uploadedImages.length} images have been successfully
+                        processed!
+                    </span>
+                    <button
+                        type="button"
+                        onClick={handleDownloadAll}
+                        className="mb-4 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300"
+                    >
+                        Download processed images
+                    </button>
+                </>
+            )}
         </main>
     );
 }
