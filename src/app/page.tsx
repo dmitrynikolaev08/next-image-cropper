06d8910b20446a53ee725c7bb1d7b27078acefaa
Dmitrii Nikolaev <dmitrynikolaev08@gmail.com>
2024-01-22 07:33:08 +0100

    ui fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
             });
 
             setLoading(true);
+            setError(null);
 
             const res = await fetch('/api/crop-images', {
                 method: 'POST',
